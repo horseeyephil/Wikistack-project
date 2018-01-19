eb6bb74bb59f6822350c9359e47c77de9bfba275
Philip Gingerich <philipygingerich@gmail.com>
2018-01-19 17:08:54 -0500

    Ignoring modules

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
-
+const env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 app.use(express.static('public'));
