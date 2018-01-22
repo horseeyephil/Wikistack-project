b6ead384eda7a0a3132815e56655ff7dba817538
Philip Gingerich <philipygingerich@gmail.com>
2018-01-22 11:49:03 -0500

    Successfully routing

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+
 const env = nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
+
 app.use(express.static('public'));
+
+const models = require('./models/');
+const routes = require('./routes/index');
+
+models.db.sync({force:true}).then(function(){
+    app.listen(3000);
+})
+.catch(console.error.bind(console));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use('/*',function(req,res,next){
+    console.log('Status Code ', res.statusCode);
+    next();
+});
+
+
+// app.get('/', function (req, res) {
+//     res.render( 'index')
+//   });
+
+
+app.use('/', routes);
--- models/index.js
+++ models/index.js
@@ -1,2 +1,27 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack',{
+    logging : false
+});
+
+const Page = db.define('page', {
+    title: { type: Sequelize.STRING, allowNull: false},
+    urlTitle: { type: Sequelize.STRING, allowNull: false},
+    content: { type: Sequelize.TEXT, allowNull: false},
+    status: { type:Sequelize.ENUM('open', 'closed')},
+})
+
+const User = db.define('user', {
+    name: { type: Sequelize.STRING, allowNull: false, notEmpty: true},
+    email: { type: Sequelize.STRING, allowNull: false, validate: {isEmail: true}},
+})
+
+
+
+
+
+module.exports = {
+    db: db,
+    page: Page,
+    user: User
+}
+
